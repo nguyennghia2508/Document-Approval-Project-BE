f5a4a5d4a74378f67796c137b045479878622d16
Nguyen Thanh Nghia <binlatao3@gmail.com>
2024-03-21 10:35:31 +0700

    update

---
+++ Document-Approval-Project-BE/Controllers/DocumentApprovalsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Document_Approval_Project_BE.Models;
+
+namespace Document_Approval_Project_BE.Controllers
+{
+    [RoutePrefix("api/documentapproval")]
+    public class DocumentApprovalsController : ApiController
+    {
+        private readonly ProjectDBContext db = new ProjectDBContext();
+        [NonAction]
+        public string GetFilePath(string path)
+        {
+            if(!path.Equals(""))
+                return Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files\\" + path);
+            return Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files");
+        }
+
+        public class DocumentData
+        {
+            public DocumentApproval Document { get; set; }
+            public DocumentApprovalFile File { get; set; }
+        }
+
+
+        [HttpPost]
+        [Route("add")]
+        public IHttpActionResult AddDocument([FromBody] DocumentData documentData)
+        {
+            if (documentData == null || documentData.Document == null)
+            {
+                return BadRequest("Request body is empty or document data is missing.");
+            }
+
+            DocumentApproval document = documentData.Document;
+            DocumentApprovalFile file = documentData.File;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (file == null)
+            {
+                db.DocumentApprovals.Add(document);
+                try
+                {
+                    db.SaveChanges();
+                    return Ok(new
+                    {
+                        state = "true",
+                        msg = "Add document success"
+
+                    });
+                }
+                catch (DbUpdateException)
+                {
+                    return InternalServerError();
+                }
+            }
+            return Ok(new
+            {
+                state = "true",
+                msg = "nothing"
+
+            });
+        }
+
+    }
+}
---
+++ Document-Approval-Project-BE/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Document_Approval_Project_BE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+namespace Document_Approval_Project_BE.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        private readonly ProjectDBContext db = new ProjectDBContext();
+        // GET: api/user/{email}
+        [HttpPost]
+        [Route("login")]
+        public IHttpActionResult Login([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Request body is empty.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var userInfo = db.Users.SingleOrDefault(p => p.Email == user.Email && p.Password == user.Password);
+            if (userInfo == null)
+            {
+                return Ok(new
+                {
+                    state = "false",
+                    msg = "Invalid email or password"
+
+                });
+            }
+            return Ok(new
+            {
+                state = "true",
+                userInfor = userInfo
+            });
+        }
+    }
+}

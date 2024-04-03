1e57c29a8a5d962d39f0fe7b3e9a9b082d570833
Nguyen Thanh Nghia <binlatao3@gmail.com>
2024-04-04 00:06:09 +0700

    update comment controller

---
+++ Document-Approval-Project-BE/Migrations/202404030810286_update-db-v7.cs
@@ -0,0 +1,18 @@
+﻿namespace Document_Approval_Project_BE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedbv7 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.DocumentApprovalComments", "CommentTime");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.DocumentApprovalComments", "CommentTime", c => c.String());
+        }
+    }
+}
---
+++ Document-Approval-Project-BE/Migrations/202404030828549_update-db-v8.cs
@@ -0,0 +1,18 @@
+﻿namespace Document_Approval_Project_BE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedbv8 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DocumentApprovalComments", "ApprovalPersonName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.DocumentApprovalComments", "ApprovalPersonName");
+        }
+    }
+}
